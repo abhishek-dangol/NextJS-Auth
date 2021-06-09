08d9b1b1c562e03ed0eb1d60b0c986477bb4ac3b
Abhishek Dangol <abhi.dangol@gmail.com>
2021-06-08 23:54:32 -0500

    added signup requests

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,30 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+  const data = req.body;
+  const { email, password } = data;
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({ message: "Invalid password!" });
+    return;
+  }
+  const client = await connectToDatabase();
+  const db = client.db();
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+  res.status(201).json({ message: "Created user!" });
+}
+
+export default handler;
