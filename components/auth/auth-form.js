c96eaf804b67a6ff83ec8b8217585e109ba57e96
Abhishek Dangol <abhi.dangol@gmail.com>
2021-06-28 23:58:26 -0500

    update next auth

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/client";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -34,7 +35,12 @@ function AuthForm() {
     //optional: Add validation
 
     if (isLogin) {
-      // log user in
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
