0bcd100925d2552087a3d50958e2cc05bf6458f6
Abhishek Dangol <abhi.dangol@gmail.com>
2021-07-01 00:01:08 -0500

    protected auth and profile page

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -22,6 +23,7 @@ function AuthForm() {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -40,7 +42,9 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword,
       });
-      console.log(result);
+      if (!result.error) {
+        router.replace("/profile");
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,23 @@
-import AuthForm from '../components/auth/auth-form';
+import AuthForm from "../components/auth/auth-form";
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter;
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return <AuthForm />;
 }
 
